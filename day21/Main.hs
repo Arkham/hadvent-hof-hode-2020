33daf7dcd2339718ee7c724b7e9ff229c261c528
Ju Liu <ju@noredink.com>
2020-12-24 22:39:18 +0000

    Finish day 21

--- day21/Main.hs
+++ day21/Main.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import Data.Bifunctor (second)
 import qualified Data.HashMap.Strict as HM
 import qualified Data.List as List
 import qualified Data.Set as Set
@@ -18,11 +19,16 @@ main = do
       print err
     Right foods -> do
       let summary = buildAllergenSummary foods
+      print summary
       let allergenIngredients = mconcat $ HM.elems summary
       print $
         length $
           filter (not . (`Set.member` allergenIngredients)) $
             concatMap ingredients foods
+      let ingredientsWithAllergens = findIngredientsWithAllergens summary
+      print $
+        List.intercalate "," $
+          map snd $ List.sort ingredientsWithAllergens
 
 data Food = Food
   { ingredients :: [String],
@@ -43,6 +49,17 @@ buildAllergenSummary =
               allergens
     )
     HM.empty
+
+findIngredientsWithAllergens :: HM.HashMap String (Set.Set String) -> [(String, String)]
+findIngredientsWithAllergens summary =
+  findResult (sortIngredients $ HM.toList summary) []
+  where
+    sortIngredients = List.sortOn (length . snd)
+    findResult [] acc = acc
+    findResult ((firstIng, firstAllergens) : rest) acc =
+      let firstAllergen = head $ Set.toList firstAllergens
+          newRest = map (second (Set.delete firstAllergen)) rest
+       in findResult (sortIngredients newRest) ((firstIng, firstAllergen) : acc)
 
 foodParser :: Parser Food
 foodParser = do
